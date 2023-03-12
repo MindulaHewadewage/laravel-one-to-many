66ec4855a344cd6c75e487d441e0fabe8da9ef48
MindulaHewadewage <mindufernando@gmail.com>
2023-03-12 20:54:23 +0100

    fixed migration

--- database/migrations/2023_03_12_193311_add_foreign_type_id_on_projects_table.php
+++ database/migrations/2023_03_12_193311_add_foreign_type_id_on_projects_table.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            // definisco la colonna
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+
+            // definisco la relazione
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null');
         });
     }
 
@@ -22,7 +26,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropForeign('projects_type_id_foreign');
+            $table->dropColumn('type_id');
         });
     }
 };
